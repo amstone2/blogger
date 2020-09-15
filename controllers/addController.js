251c46e9c9b26b5ca3ecffd37a5320644c47d145
Debian <amstone2@millersville.edu>
2020-09-15 22:27:26 +0000

    Adds foundation for the app

---
+++ controllers/addController.js
@@ -0,0 +1,3 @@
+module.exports.add = function(req, res){
+  res.render('add', { title: 'Blog Add Page' });
+}; 
---
+++ controllers/listController.js
@@ -0,0 +1,3 @@
+module.exports.list = function(req, res){
+  res.render('list', { title: 'Blog list Page' });
+}; 
